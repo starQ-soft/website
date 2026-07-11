03781acb415b931d4a16cca481137de9aef36d7b
pairakigames <pairakigame@gmail.com>
2026-07-10 17:19:37 -0700

    lock file

--- starq_soft/src/router.tsx
+++ starq_soft/src/router.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from "./App"
 import Lsc from "./pages/Lsc"
 import { LanguageProvider } from "./LanguageContext"
@@ -8,12 +8,14 @@ import { LanguageProvider } from "./LanguageContext"
 const AppRouter: React.FC = () => {
   return (
     <LanguageProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/love-startup-cofounder" element={<Lsc />} />
-        </Routes>
-      </BrowserRouter >
+      <React.StrictMode>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/love-startup-cofounder" element={<Lsc />} />
+          </Routes>
+        </HashRouter>
+      </React.StrictMode>,
     </LanguageProvider>
   )
 }
